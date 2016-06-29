0dd84cdb07e9605579d31fedae5578e9cb49d1b1
EKyle <elouisekyle@gmail.com>
2016-06-29 15:09:03 +0100

    Added Emma to output line

--- src/Engine.java
+++ src/Engine.java
@@ -3,8 +3,10 @@
  */
 public class Engine {
 
+    private static String Emma = "Emma";
+
     public static void main(String args[])
     {
-        System.out.println("Hello World!!!!!");
+        System.out.println("Hello World" + " " + Emma);
     }
 }
